a15c13e38037cf7e3c7d771b100aeae63f72ff5f
EC2 Default User <ec2-user@ip-172-31-25-149.ec2.internal>
2022-02-08 11:22:15 +0000

    [Add]TOPページのレイアウト

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+end
